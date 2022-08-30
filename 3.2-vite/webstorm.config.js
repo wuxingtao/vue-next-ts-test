d9a42a7d5a66467a6240a7b6b05b5f7aaac861c1
xingtao <xingtaowap@gmail.com>
2022-08-30 21:09:43 +0800

    [3.2] 改用pnpm 生成node_modules

---
+++ 3.2-vite/webstorm.config.js
@@ -0,0 +1,21 @@
+const resolve = dir => require('path').join(__dirname, dir)
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx', '.css', '.less', '.vue', '.ts'],
+    alias: {
+      '@': resolve('./src'),
+      '@views': resolve('src/views'),
+      '@api': resolve('src/services/api'),
+      '@comps': resolve('src/components'),
+      '@common': resolve('src/common'),
+      '@router': '@/router/',
+      '@assets': '@/assets',
+      '@utils': resolve('src/utils'),
+      '@export': '@/export',
+      '@client': '@/views/client',
+      '@waybill': '@/views/waybill',
+      '@enum': '@/enum/'
+    }
+  }
+}
