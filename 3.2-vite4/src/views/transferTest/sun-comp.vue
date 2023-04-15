bd4ea34a3a5b6bcd369b48522d0e04565b19db21
xingtao <xingtaowap@gmail.com>
2023-04-15 11:21:29 +0800

    [3.2-vite4] 更新defineProps note

--- 3.2-vite4/src/views/transferTest/sun-comp.vue
+++ 3.2-vite4/src/views/transferTest/sun-comp.vue
@@ -3,12 +3,18 @@
     <p>
       sun props msg: {{msg}}
     </p>
+    <p>
+      sun props desc: {{desc}}
+    </p>
   </div>
 </template>
 
 <script setup lang='ts'>
-import { reactive } from 'vue'
-defineProps<{msg:string}>()
+import { reactive,withDefaults } from 'vue'
+const props = withDefaults(defineProps<{msg:string,desc?:string}>(),{
+  msg: '默认值',
+  desc: '默认值'
+})
 
 const state = reactive({})
 </script>
